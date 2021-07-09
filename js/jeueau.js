99677377c060018f87497ba8d7b4d17c5a7f0fe4
Camille Bravo <camille.bravotholon@gmail.com>
2021-07-09 15:22:39 +0200

    js drag'n drop jeueau

--- js/jeueau.js
+++ js/jeueau.js
@@ -1,53 +1,68 @@
-function dragStart(event) {
-    event.dataTransfer.setData("Text", event.target.id);
-}
-
-    function allowDrops(event) {
+function allowDrop(event) {
     event.preventDefault();
-    }
-
-    function drop(event) {
-        event.preventDefault();
-        var data = event.dataTransfer.getData("Text");
-        event.target.appendChild(document.getElementById(data));
-            if(event.target.classList.contains('dropins')){
-                div.classList.add("green")
-            }
-            else{
-                div.classList.add("red")
-            }
-    }
-
+}
 function dragStart(event) {
     event.dataTransfer.setData("Text", event.target.id);
+    let data = event.dataTransfer.getData("Text");
+    console.log(data)
 }
-    
-    function allowDropl(event) {
-        event.preventDefault();
-    }
-    
-    function drop(event) {
-        event.preventDefault();
-        var data = event.dataTransfer.getData("Text");
-        event.target.appendChild(document.getElementById(data));
-            if(event.target.classList.contains('dropinl')){
-                console.log('GG')
-            }
-    }
 
-function dragStart(event) {
-    event.dataTransfer.setData("Text", event.target.id);
+//Fonction pour les solides
+function allowDrops(event) {
+    event.preventDefault();
 }
-    
-    function allowDropg(event) {
-        event.preventDefault();
+function drops(event) {
+    event.preventDefault();
+    let data = event.dataTransfer.getData("Text");
+    console.log(data)
+    event.target.appendChild(document.getElementById(data));
+    if(event.target.id  == document.getElementById(data).getAttribute('data-divs')){
+        event.target.appendChild(document.getElementById(data))
+        document.getElementById(data).setAttribute('class', 'green')
+        console.log("green divs" + event.target.id)
     }
-    
-    function drop(event) {
-        event.preventDefault();
-        var data = event.dataTransfer.getData("Text");
-        event.target.appendChild(document.getElementById(data));
-            if(event.target.classList.contains('droping')){
-                console.log('GG')
-            }
+    else{
+        document.getElementById(data).setAttribute('class', 'red')
+        console.log("red divs" + event.target.id)
     }
+}
+
+//Fonction pour les liquides
+function allowDropl(event) {
+    event.preventDefault();
+}
+function dropl(event) {
+    event.preventDefault();
+    let data = event.dataTransfer.getData("Text");
+    console.log(data)
+    event.target.appendChild(document.getElementById(data));
+    if(event.target.id  == document.getElementById(data).getAttribute('data-divl')){
+        event.target.appendChild(document.getElementById(data))
+        document.getElementById(data).setAttribute('class', 'green')
+        console.log("green divl" + event.target.id)
+    }
+    else{
+        document.getElementById(data).setAttribute('class', 'red')
+        console.log("red divl" + event.target.id)
+    }
+}
+
+//Fonction pour le gaz
+function allowDropg(event) {
+    event.preventDefault();
+}
+function dropg(event) {
+    event.preventDefault();
+    let data = event.dataTransfer.getData("Text");
+    console.log(data)
+    event.target.appendChild(document.getElementById(data));
+    if(event.target.id  == document.getElementById(data).getAttribute('data-divg')){
+        event.target.appendChild(document.getElementById(data))
+        document.getElementById(data).setAttribute('class', 'green')
+        console.log("green divg" + event.target.id)
+    }
+    else{
+        document.getElementById(data).setAttribute('class', 'red')
+        console.log("red divg" + event.target.id)
+    }
+}
